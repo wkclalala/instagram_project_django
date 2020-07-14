05208133f5016ae752fe3f91e838876aabe3abe9
Kaichen <kw573@cornell.edu>
2020-07-14 16:15:14 -0400

    add whitenoise

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'posts'
 LOGOUT_REDIRECT_URL = 'posts'
